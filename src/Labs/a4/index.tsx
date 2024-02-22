2016d2f1b91512603c9ebad224206031d1bf7146
Zining <ziningxie10@gmail.com>
2024-02-21 21:08:18 -0500

    JavaScript and React.js Assignment 3

---
+++ src/Labs/a4/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+// import JavaScript from "./JavaScript";
+function Assignment4() {
+  return (
+    <div className="container">
+      <h1>Assignment 4</h1>
+      {/* <JavaScript /> */}
+    </div>
+  );
+}
+export default Assignment4;
